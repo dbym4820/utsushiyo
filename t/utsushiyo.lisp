bbbf4bca71299b53b11a1bb6158253d0c0eb7ceb
Tomoki Aburatani <aburatanitomoki@gmail.com>
2018-05-13 03:20:45 +0900

    add simple test

--- t/utsushiyo.lisp
+++ t/utsushiyo.lisp
@@ -5,9 +5,23 @@
 (in-package :utsushiyo-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :utsushiyo)' in your Lisp.
+(setf *enable-colors* nil)
 
-(plan nil)
+(plan 1)
 
-;; blah blah blah.
-
+(subtest "project-env test"
+	 (let ((test-instance
+		 (make-instance 'project-env
+				:project-env-name "utsushiyo"
+				:project-root-path (namestring
+						    (directory-namestring
+						     (asdf:system-relative-pathname 'utsushiyo "")))
+				:config-dir (concagtenate 'string utsushiyo::+user-home-dirname+ ".utsushiyo/"))))
+	   (ok (bootstrap))
+	   (is-type (get-help "example" (make-project-env "utsushiyo")) 'string)
+	   (is-type (project-env-name test-instance) 'string)
+	   (is-type (project-root-path test-instance) 'string)
+	   (is-type (config-dir test-instance) 'string)))
+	   
+		 
 (finalize)
