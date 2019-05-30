5e30fea58d676d9dcc1b1550ad12a04d74c65026
Tomoki Aburatani <aburatanitomoki@gmail.com>
2019-05-30 10:45:09 +0900

    fix test

--- t/utsushiyo.lisp
+++ t/utsushiyo.lisp
@@ -7,21 +7,25 @@
 ;; NOTE: To run this test file, execute `(asdf:test-system :utsushiyo)' in your Lisp.
 (setf *enable-colors* nil)
 
-(plan 1)
+(plan 2)
 
 (subtest "project-env test"
+	 (ok (not (bootstrap))))
+
+(subtest "environment create completition test"
 	 (let ((test-instance
-		 (make-instance 'project-env
+		 (make-instance 'utsushiyo::project-env
 				:project-env-name "utsushiyo"
 				:project-root-path (namestring
 						    (directory-namestring
-						     (asdf:system-relative-pathname 'utsushiyo "")))
+						     (asdf:system-relative-pathname 'utsushiyo "src/utsushiyo-default")))
 				:config-dir (concatenate 'string utsushiyo::+user-home-dirname+ ".utsushiyo/"))))
-	   (bootstrap)
-	   (is-type (get-help (make-project-env "utsushiyo") "example") 'string)
-	   (is-type (project-env-name test-instance) 'string)
-	   (is-type (project-root-path test-instance) 'string)
-	   (is-type (config-dir test-instance) 'string)))
-
+	   (is-type (get-help (make-project-env "utsushiyo") "bin-general-help") 'string)
+	   (is (utsushiyo::project-env-name test-instance) "utsushiyo")
+	   (is-type (utsushiyo::project-root-path test-instance) 'string)
+	   (is (utsushiyo::config-dir test-instance)
+	       (concatenate 'string
+			    utsushiyo::+user-home-dirname+
+			    ".utsushiyo/"))))
 		 
 (finalize)
